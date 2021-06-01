6e77d379d3dccd61a6b74cb920ce74c03e83c1a7
Roman Kozko <romanchek_bananchek@mail.ru>
2021-06-01 21:58:13 +0300

    set up project

---
+++ ChatServer/BSDSocketWrapperMac.framework/Versions/A/Headers/BSDSocketWrapperMac.h
@@ -0,0 +1,18 @@
+//
+//  BSDSocketWrapperMac.h
+//  BSDSocketWrapperMac
+//
+//  Created by Admin on 21.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSDSocketWrapperMac.
+FOUNDATION_EXPORT double BSDSocketWrapperMacVersionNumber;
+
+//! Project version string for BSDSocketWrapperMac.
+FOUNDATION_EXPORT const unsigned char BSDSocketWrapperMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSDSocketWrapperMac/PublicHeader.h>
+
+
---
+++ ChatServer/ChatServer/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ChatServer
+//
+//  Created by Admin on 01.06.2021.
+//
+
+import Foundation
+
+print("Hello, World!")
+
