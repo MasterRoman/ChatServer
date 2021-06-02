783b7a48ddd6cb42238f45dafd8c33453c93e15e
Roman Kozko <romanchek_bananchek@mail.ru>
2021-06-02 18:58:26 +0300

    add data struct for users

---
+++ ChatServer/ChatServer/Storage/UserStorage.swift
@@ -0,0 +1,63 @@
+//
+//  UserStorage.swift
+//  ChatServer
+//
+//  Created by Admin on 02.06.2021.
+//
+
+import Foundation
+
+class UserData{
+    private var chats : Dictionary<UUID,Chat>
+    private var messages : Dictionary<UUID,ChatBody>
+    private var contacts : [Contact]
+    private var credentials : Credentials
+    
+    init(with login : String) {
+        self.chats = Dictionary<UUID,Chat>()
+        self.messages = Dictionary<UUID,ChatBody>()
+        self.contacts = [Contact]()
+        self.credentials = Credentials(login: login, password: "")
+    }
+    
+    func addChat(chat : Chat){
+        let id = chat.chatBody.chatId
+        chats[id] = chat
+        self.messages[id] = ChatBody(chatId: id, messages: [])
+    }
+    
+    func removeChat(by id : UUID){
+        chats[id] = nil
+    }
+    
+    func addMessages(messages : ChatBody){
+        let id = messages.chatId
+        let chat = chats[id]
+        guard let chatToAdd = chat else { return }
+        chatToAdd.chatBody.messages.append(contentsOf: messages.messages)
+        
+        self.messages[id]?.messages.append(contentsOf: messages.messages)
+    }
+    
+    func addContact(contact : Contact){
+        contacts.append(contact)
+    }
+    
+    func removeContact(by nickname : String){
+        let index = contacts.firstIndex(where: {$0.login == nickname})
+        if let index = index{
+            contacts.remove(at: index)
+        }
+    }
+    
+    func setPassword(password : String){
+        credentials.password = password
+    }
+    
+    func checkPassword(password : String) -> Bool{
+        if credentials.password == password{
+            return true
+        }
+        return false
+    }
+}
